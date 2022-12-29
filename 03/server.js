c5aced3aed77d39adb13cd2a908da10a058b0c3f
Abhinav Sahu <abhinav.sahu@healogics.com>
2022-12-29 13:09:13 +0530

    web server end

--- 03/server.js
+++ 03/server.js
@@ -1,21 +1,44 @@
 const http = require('http');
 const path = require('path');
-const fs = require('fs');
+const fsPromises = require('fs').promises;
 
 const PORT = 3305;
 
-const server  = http.createServer((req, res) => {
 
-    console.log(req.url, req.method);
-
+const serverCsv = (req, res) => {
     res.setHeader("Content-Type", "text/csv");
     res.setHeader("Content-Disposition", "attachment;filename=report.csv");
     res.writeHead(200);
     res.end(`id,name,email\n1,Sammy Shark,shark@ocean.com`);
-   
+
+}
+
+const serverHtml = (req, res) => {
+
+    fsPromises.readFile(path.join(__dirname, 'view', 'index.html'), 'utf-8').then((data) => {
+
+        res.setHeader("Content-Type", "text/html");
+        res.writeHead(200);
+        res.end(data);
+    }).catch(err => {
+        res.writeHead(500);
+        res.end(err);
+        return;
+    });
+
+
+
+}
+const server = http.createServer((req, res) => {
+
+    console.log(req.url, req.method);
+    //serverCsv(req,res);
+    serverHtml(req, res);
+
+
 })
 
-server.listen(PORT, () =>{
+server.listen(PORT, () => {
 
     console.log(`Server is running on port ${PORT}`);
 })
